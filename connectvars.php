277977020b5a9b9624d16766fb305fc4184784cd
Repnikum <repnikum@gmail.com>
2016-03-12 16:53:20 +0300

    стерты лишние файлы 2

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'a162547.mysql.mchost.ru');
-  define('DB_USER', 'a162547_1');
-  define('DB_PASSWORD', 'u7EVfZt8-5TFDi');
-  define('DB_NAME', 'a162547_1');
-?>
